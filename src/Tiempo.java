17091980252f40518db32177a4375f3b618de100
JESUS SANCHEZ <L24171273@culiacan.tecnm.mx>
2025-09-10 06:17:00 +0000

    correcciones

--- src/Tiempo.java
+++ src/Tiempo.java
@@ -1,9 +1,87 @@
 //Tiempo.java
-//Clase que implementa la clase tiempo
+//Implementación la clase tiempo
 public class Tiempo {
+    //Atributos
     private int horas;
     private int minutos;
     private int segundos;
+    //Constructor por defecto
+    public Tiempo()
+    {
+        horas = 0;
+        minutos = 0;
+        segundos = 0;
+    }
+    //Constructor copia
+    public Tiempo (Tiempo t)
+    {
+        this.horas=t.horas;
+        this.minutos=t.minutos;
+        this.segundos=t.segundos;
+    }
+    // Constructores
+    public Tiempo(int horas, int minutos, int segundos) 
+    {
+        setHoras(horas);
+        setMinutos(minutos);
+        setSegundos(segundos);
+    }
+    public Tiempo(int horas, int minutos) 
+    {
+        this(horas, minutos,0 );
+    }
+    public Tiempo(int segundos) 
+    {
+        this(0, 0, segundos);
+    }
+    //metodo set/get
+    public void setHoras(int horas)    {
+        if (horas >= 0 && horas < 24)
+        {
+            this.horas = horas;
+        }
+        else
+        {
+           horas = 0;
+        }
+    }
+    public void setMinutos(int minutos)
+    {
+        if (minutos >= 0 && minutos < 60)
+        {
+            this.minutos = minutos;
+        }
+        else
+        {
+            this.minutos = 0;
+           
+        }
+    }
+    public void setSegundos(int segundos)
+    {
+        if (segundos >= 0 && segundos < 60)
+        {
+            this.segundos = segundos;
+        }
+        else
+        {
+            this.segundos = 0;
+           
+        }
+    }
+    public int getHoras()
+    {
+        return horas;
+    }
+    public int getMinutos()
+    {
+        return minutos;
+    }
+    public int getSegundos()
+    {
+        return segundos;
+    }
+
     //metodo tostring
     public String toString() {
         String resultado=String.format("%02d:%02d:%02d", horas, minutos, segundos);
--- src/prog0201.java
+++ src/prog0201.java
@@ -1,46 +1,51 @@
+//Prog0201.java
 public class prog0201
 {
     public static void main(String[] args)
+{
     //Crear un objeto clase tiempo
     Tiempo t1=new Tiempo();
     //Asignar valores a los atributos
-    t1.horas=8;
-    t1.minutos=36;
-    t1.segundos=20;
+    t1.setHoras(8);
+    t1.setMinutos(36);
+    t1.setSegundos(20);
     //Mostrar valores de los atributos
-    System.out.println("Horas: "t1.horas);
-    System.out.println("Minutos: "t1.minutos);  
-    System.out.println("Segundos: "t1.segundos);
+    System.out.println("Horas: " + t1.getHoras());
+    System.out.println("Minutos: " + t1.getMinutos());
+    System.out.println("Segundos: " + t1.getSegundos());
     //Mostrar el tiempo con el metodo toString
     System.out.println(t1.toString());
-    Tiempo t2=new Time
-    {
-        System.out.println("Hola a todos!");
-    }
+    Tiempo t2= new Tiempo();
+    t2.setHoras(10);
+    t2.setMinutos(15);
+    t2.setSegundos(30);
 
-    //metodo set/get
-    if (horas >= 0 && horas < 23)
-    {
-        this.horas = horas;
-    }
-    else
-    horas = 0;
-    {
-        System.out.println("Valor de horas no valido");
-    }
-    public int getHoras()
-    {
-        return horas;
-        
-    }
-     public int getminutos()
-    {
-        return minutos;
-        
-    }
-     public int getsegundos()
-    {
-        return segundos;
-        
-    }
+    System.out.println(t1);
+    System.out.println(t2);
+    Tiempo t3= new Tiempo();
+    t3.setHoras(0);
+    t3.setMinutos(0);
+    t3.setSegundos(0);
+
+    Tiempo t4=new Tiempo (8,19,45);
+    Tiempo t5=new Tiempo (8,19);
+    Tiempo t6=new Tiempo (8);
+
+    System.out.println(t4);
+    System.out.println(t5);
+    System.out.println(t6);
+    Tiempo t7=t6;
+    System.out.println(t7);
+    
+    t7.setMinutos(35);
+    System.out.println(t6);
+    System.out.println(t7);
+    Tiempo t8 = new Tiempo(t7);
+    System.out.println(t8);
+    t8.setSegundos(50);
+    System.out.println(t7);
+    System.out.println(t8);
+    System.out.println(t6);
 }
+}
+
